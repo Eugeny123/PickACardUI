1de98acb078a27ddcedf82398e025354fe75cce0
Eugeny <sgenya72@gmail.com>
2022-10-04 13:26:20 +0300

    added event handler

--- PickACardUI/MainWindow.xaml.cs
+++ PickACardUI/MainWindow.xaml.cs
@@ -25,6 +25,14 @@ namespace PickACardUI
             InitializeComponent();
         }
 
-        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            listOfCards.Items.Clear();
+            foreach (string card in pickedCards)
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
     }
 }
